448b8157d712ba9757a9bc5118ebc3e9976c2265
Ibrahim Zaqout <EngIbrahimZaqout@outlook.com>
2020-09-12 20:32:29 +0100

    first

--- app/src/main/java/com/coders/al_fozan/MainActivity.java
+++ app/src/main/java/com/coders/al_fozan/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,27 +21,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-            initList();
-            Spinner spinnerCountries = findViewById(R.id.spinner_countries);
-            mAdapter = new CountryAdapter(this, mCountryList);
-            spinnerCountries.setAdapter(mAdapter);
-            spinnerCountries.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    CountryItem clickedItem = (CountryItem) parent.getItemAtPosition(position);
-                    String clickedCountryName = clickedItem.getCountryName();
-                    Toast.makeText(MainActivity.this, clickedCountryName + " selected", Toast.LENGTH_SHORT).show();
-                }
-                @Override
-                public void onNothingSelected(AdapterView<?> parent) {
-                }
-            });
-        }
-        private void initList() {
-            mCountryList = new ArrayList<>();
-            mCountryList.add(new CountryItem("India", R.drawable.india));
-            mCountryList.add(new CountryItem("China", R.drawable.china));
-            mCountryList.add(new CountryItem("USA", R.drawable.usa));
-            mCountryList.add(new CountryItem("Germany", R.drawable.germany));
+
+
+        initList();
+        Spinner spinnerCountries = findViewById(R.id.spinner_countries);
+        mAdapter = new CountryAdapter(this, mCountryList);
+        spinnerCountries.setAdapter(mAdapter);
+        spinnerCountries.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                CountryItem clickedItem = (CountryItem) parent.getItemAtPosition(position);
+                String clickedCountryName = clickedItem.getCountryName();
+                Toast.makeText(MainActivity.this, clickedCountryName + " selected", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+    private void initList() {
+        mCountryList = new ArrayList<>();
+        mCountryList.add(new CountryItem("India", R.drawable.india));
+        mCountryList.add(new CountryItem("China", R.drawable.china));
+        mCountryList.add(new CountryItem("USA", R.drawable.usa));
+        mCountryList.add(new CountryItem("Germany", R.drawable.germany));
     }
 }
+
+
