542895b2f0260f62140d3ce7933978e5b2ee1ac3
Ibrahim Zaqout <EngIbrahimZaqout@outlook.com>
2020-09-16 10:45:40 +0100

    it

---
+++ app/src/main/java/com/coders/al_fozan/Order.java
@@ -0,0 +1,43 @@
+package com.coders.al_fozan;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Order extends AppCompatActivity {
+    TextView en, enV, ar, arV;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order);
+
+        en = findViewById(R.id.text_english);
+        enV = findViewById(R.id.text_english_v);
+        ar = findViewById(R.id.text_arabic);
+        arV = findViewById(R.id.text_arabic_v);
+
+        en.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enV.setVisibility(View.VISIBLE);
+                ar.setVisibility(View.VISIBLE);
+                arV.setVisibility(View.GONE);
+                en.setVisibility(View.GONE);
+            }
+        });
+        ar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arV.setVisibility(View.VISIBLE);
+                en.setVisibility(View.VISIBLE);
+                enV.setVisibility(View.GONE);
+                ar.setVisibility(View.GONE);
+            }
+        });
+
+
+
+
+    }}
