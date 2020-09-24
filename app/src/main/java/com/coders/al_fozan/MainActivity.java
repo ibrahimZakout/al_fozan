6ba9b193c93d4a9daa7e94d0aa8f0f49bfff7e0e
Ibrahim Zaqout <EngIbrahimZaqout@outlook.com>
2020-09-24 01:21:12 +0100

    ct

--- app/src/main/java/com/coders/al_fozan/MainActivity.java
+++ app/src/main/java/com/coders/al_fozan/MainActivity.java
@@ -5,20 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<CountryItem> mCountryList;
     private CountryAdapter mAdapter;
+    Button button4,save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+      button4 = findViewById(R.id.button4);//this for bottom sheet
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
+                bottomSheetDialog.setContentView(R.layout.bottom_sheet);
+                bottomSheetDialog.setCanceledOnTouchOutside(false);
+
+              save =  bottomSheetDialog.findViewById(R.id.save);
+              save.setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View view) {
+                    Toast.makeText(getBaseContext(),"hi",Toast.LENGTH_LONG).show();
+                  }
+              });
+
+
+                bottomSheetDialog.show();
+            }
+        });
+
 
 
 
